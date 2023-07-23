7fa7f65895ab1d32cda998f34ecbb4b1786c4870
Joshua Nguyen <jnguye17@terpmail.umd.edu>
2023-07-22 21:06:17 -0400

    Assignment 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return <h1>REACT COURSE</h1>;
+}
+
+function Body() {
+  return (
+    <div>
+      <p>hello world</p>
+      <ul>
+        <li>list element 1</li>
+        <li>list element 2</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  return <p>Happy Coding!</p>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
